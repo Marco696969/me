d018da9cde39d54d73556b4466b2b12ef1e06594
marco romeo <marcoromeo@optusnet.com.au>
2019-06-14 12:43:04 +1000

    homework

--- week2/exercise0.py
+++ week2/exercise0.py
@@ -91,7 +91,7 @@ def shout_with_a_number(a_string, a_number):
 
     HINT: Lookup how to cast a_number to a string or lookup how to use string formatting in python
     """
-    the_answer = (HI, 9)
+    the_answer = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", + 9)
 
     return the_answer
 
